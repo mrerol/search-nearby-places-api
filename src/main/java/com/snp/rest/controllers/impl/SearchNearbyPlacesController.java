aa7ffad9a8375afae4b3156dc516169422af305a
mr.erol <mr.erol@outlook.com>
2023-12-20 13:50:06 +0300

    cors

--- src/main/java/com/snp/rest/controllers/impl/SearchNearbyPlacesController.java
+++ src/main/java/com/snp/rest/controllers/impl/SearchNearbyPlacesController.java
@@ -8,10 +8,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class SearchNearbyPlacesController implements SearchNearbyPlacesControllerInterface {
 
   private static final Logger logger = LoggerFactory.getLogger(SearchNearbyPlacesController.class);
